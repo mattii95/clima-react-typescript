1ee0a4d44e5af4dac96f45b2141754bced4b2f4c
Matias Courroux <mcourroux@hotmail.com>
2024-07-23 16:18:54 -0300

    proyecto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css'
+import Alert from './components/Alert/Alert'
+import Form from './components/Form/Form'
+import Spinner from './components/Spinner/Spinner'
+import WeatherDetail from './components/WeatherDetail/WeatherDetail'
+import useWeather from './hooks/useWeather'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const { weather, loading, notFound, fetchWeather, hasWeatherData } = useWeather()
+
+    return (
+        <>
+            <h1 className={styles.title}>Buscador de Clima</h1>
+            <div className={styles.container}>
+                <Form
+                    fetchWeather={fetchWeather}
+                />
+                {loading && <Spinner />}
+                {hasWeatherData && <WeatherDetail weather={weather} />}
+                {notFound && <Alert>Ciudad no Encontrada</Alert>}
+            </div>
+        </>
+    )
 }
 
 export default App
